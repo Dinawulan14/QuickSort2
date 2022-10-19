0441444458b118cec684183f5387dc2a170f564c
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-10-19 17:17:46 +0700

    menambahkan while (true)

--- QuickSort2/Program.cs
+++ QuickSort2/Program.cs
@@ -10,6 +10,21 @@ namespace QuickSort2
         private int mov_count = 0; //number of data movements
 
         // Number of elements in array
+        private int n;
 
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the umber of elementsin the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+        }
     }
 }
