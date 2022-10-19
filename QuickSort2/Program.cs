938954741cc93aa47436c8e294b46609d3c6a329
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-10-19 17:28:00 +0700

    Menambahkan string

--- QuickSort2/Program.cs
+++ QuickSort2/Program.cs
@@ -30,9 +30,14 @@ namespace QuickSort2
             Console.WriteLine("\n================================");
 
             //get array elements
-            for(int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < n; i++)
             {
-
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[1] = Int32.Parse(s1);
+            }
+        }
+        //swaps the element at index x with the element at i
             }
 
         }
